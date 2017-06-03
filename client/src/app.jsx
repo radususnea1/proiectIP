18698916745c29f1a282d727b596978a5991e0c5
radususnea1 <radususnea@gmail.com>
2017-06-03 19:20:23 +0300

    Fix

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -1,19 +1,19 @@
 import React from 'react';
-import ReactDom from 'react-dom';
+import ReactDOM from 'react-dom';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-// import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import routes from './appRoutes.js';
-import { Router, Route, browserHistory, IndexRoute  } from 'react-router'
+import { Router, Route, browserHistory, IndexRoute } from 'react-router'
 
 
-import Base from './components/base.jsx';
-import HomePage from './components/homePage.jsx';
-import DashboardPage from './containers/dashboardPage.jsx';
-import LoginPage from './containers/loginPage.jsx';
-import SignUpPage from './containers/signUpPage.jsx';
-import Auth from './modules/auth';
+// import Base from './components/base.jsx';
+// import HomePage from './components/homePage.jsx';
+// import DashboardPage from './containers/dashboardPage.jsx';
+// import LoginPage from './containers/loginPage.jsx';
+// import SignUpPage from './containers/signUpPage.jsx';
+// import Auth from './modules/auth';
 
 // remove tap delay, essential for MaterialUI to work properly
 // injectTapEventPlugin();
@@ -21,19 +21,18 @@ import Auth from './modules/auth';
 console.log("helloo");
 
 ReactDOM.render((
-<Router >
+  <BrowserRouter routes={routes} />
+), document.getElementById('react-app'))
+
+
+{/*<Router >
   <Route path = "/" component = {Base}>
      <IndexRoute component = {HomePage} />
      <Route path = "home" component = {DashboardPage} />
      <Route path = "about" component = {LoginPage} />
      <Route path = "contact" component = {Auth} />
   </Route>
-</Router>
-
-), document.getElementById('app'))
-
-
-
+</Router>*/}
 
   // <routes />,
   // document.getElementById('react-app')
--- client/src/appRoutes.js
+++ client/src/appRoutes.js
@@ -6,7 +6,7 @@ import LoginPage from './containers/loginPage.jsx';
 import SignUpPage from './containers/signUpPage.jsx';
 import Auth from './modules/auth';
 
-import { BrowserRouter as Router, Link, Match, Miss } from 'react-router';
+/*import { BrowserRouter as Router, Link, Match, Miss } from 'react-router';
 
 const routes = () => {
   return (
@@ -21,42 +21,43 @@ const routes = () => {
   );
 };
 
+export default routes;*/
+
+
+const routes =  {
+  component: Base,
+  childRoutes: [
+    {
+      path: '/',
+      getComponent: (location, callback) => {
+        if (Auth.isUserAuthenticated()) {
+          callback(null, DashboardPage);
+        } else {
+          callback(null, HomePage);
+        }
+      }
+    },
+    {
+      path: '/login',
+      component: LoginPage
+    },
+
+    {
+      path: '/signup',
+      component: SignUpPage
+    },
+
+    {
+      path: '/logout',
+      onEnter: (nextState, replace) => {
+        Auth.deauthenticateUser();
+
+        // change the current URL to /
+        replace('/');
+      }
+    }
+
+  ]
+};
+
 export default routes;
-
-// export default routes;
-
-// const routes =  {
-//   component: Base,
-//   childRoutes: [
-//     {
-//       path: '/',
-//       getComponent: (location, callback) => {
-//         if (Auth.isUserAuthenticated()) {
-//           callback(null, DashboardPage);
-//         } else {
-//           callback(null, HomePage);
-//         }
-//       }
-//     },
-//     {
-//       path: '/login',
-//       component: LoginPage
-//     },
-
-//     {
-//       path: '/signup',
-//       component: SignUpPage
-//     },
-
-//     {
-//       path: '/logout',
-//       onEnter: (nextState, replace) => {
-//         Auth.deauthenticateUser();
-
-//         // change the current URL to /
-//         replace('/');
-//       }
-//     }
-
-//   ]
-// };
