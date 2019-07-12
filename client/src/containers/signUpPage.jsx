5a0a5955c2141f45fab11ff472cf5dd3ec4e717d
Radu Susnea <radu.susnea@endava.com>
2019-07-12 15:58:06 +0300

    Hello my first commit

--- client/src/containers/signUpPage.jsx
+++ client/src/containers/signUpPage.jsx
@@ -8,72 +8,198 @@ class SignUpPage extends React.Component {
 
     this.state = {
       errors: {},
-      user: {
-        email: '',
-        name: '',
-        password: ''
-      }
-    };
+      user: { import React, { PropTypes } from 'react';
+    import SignUpForm from '../components/signUpForm.jsx';
 
-    this.processForm = this.processForm.bind(this);
-    this.changeUser = this.changeUser.bind(this);
-  }
 
-  processForm(event) {
-    event.preventDefault();
-    const name = encodeURIComponent(this.state.user.name);
-    const email = encodeURIComponent(this.state.user.email);
-    const password = encodeURIComponent(this.state.user.password);
-    const formData = `name=${name}&email=${email}&password=${password}`;
+    class SignUpPage extends React.Component {
+      constructor(props, context) {
+        super(props, context);
 
-    const xhr = new XMLHttpRequest();
-    xhr.open('post', '/auth/signup');
-    xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-    xhr.responseType = 'json';
-    xhr.addEventListener('load', () => {
-      if (xhr.status === 200) {
-        this.setState({
-          errors: {}
-        });
-        localStorage.setItem('successMessage', xhr.response.message);
-        this.context.router.replace('/login');
-      } else {
-        const errors = xhr.response.errors ? xhr.response.errors : {};
-        errors.summary = xhr.response.message;
+        this.state = {
+          errors: {},
+          user: {
+            email: '',
+            name: '',
+            password: ''
+          }
+        };
 
-        this.setState({
-          errors
-        });
-      }
-    });
-    xhr.send(formData);
-  }
+        this.processForm = this.processForm.bind(this);
+        this.changeUser = this.changeUser.bind(this); import React, { PropTypes } from 'react';
+        import SignUpForm from '../components/signUpForm.jsx';
 
-  changeUser(event) {
-    const field = event.target.name;
-    const user = this.state.user;
-    user[field] = event.target.value;
 
-    this.setState({
-      user
-    });
-  }
+        class SignUpPage extends React.Component {
+          constructor(props, context) {
+            super(props, context);
 
-  render() {
-    return (
-      <SignUpForm
-        onSubmit={this.processForm}
-        onChange={this.changeUser}
-        errors={this.state.errors}
-        user={this.state.user}
-      />
-    );
-  }
+            this.state = {
+              errors: {},
+              user: {
+                email: '',
+                name: '',
+                password: ''
+              }
+            };
 
-}
+            this.processForm = this.processForm.bind(this);
+            this.changeUser = this.changeUser.bind(this); import React, { PropTypes } from 'react';
+            import SignUpForm from '../components/signUpForm.jsx';
 
-SignUpPage.contextTypes = {
-  router: PropTypes.object.isRequired
-};
 
-export default SignUpPage;
+            class SignUpPage extends React.Component {
+              constructor(props, context) {
+                super(props, context);
+
+                this.state = {
+                  errors: {},
+                  user: {
+                    email: '',
+                    name: '',
+                    password: ''
+                  }
+                };
+
+                this.processForm = this.processForm.bind(this);
+                this.changeUser = this.changeUser.bind(this); import React, { PropTypes } from 'react';
+                import SignUpForm from '../components/signUpForm.jsx';
+
+
+                class SignUpPage extends React.Component {
+                  constructor(props, context) {
+                    super(props, context);
+
+                    this.state = {
+                      errors: {},
+                      user: {
+                        email: '',
+                        name: '',
+                        password: ''
+                      }
+                    };
+
+                    this.processForm = this.processForm.bind(this);
+                    this.changeUser = this.changeUser.bind(this); import React, { PropTypes } from 'react';
+                    import SignUpForm from '../components/signUpForm.jsx';
+
+
+                    class SignUpPage extends React.Component {
+                      constructor(props, context) {
+                        super(props, context);
+
+                        this.state = {
+                          errors: {},
+                          user: {
+                            email: '',
+                            name: '',
+                            password: ''
+                          }
+                        };
+
+                        this.processForm = this.processForm.bind(this);
+                        this.changeUser = this.changeUser.bind(this); import React, { PropTypes } from 'react';
+                        import SignUpForm from '../components/signUpForm.jsx';
+
+
+                        class SignUpPage extends React.Component {
+                          constructor(props, context) {
+                            super(props, context);
+
+                            this.state = {
+                              errors: {},
+                              user: {
+                                email: '',
+                                name: '',
+                                password: ''
+                              }
+                            };
+
+                            this.processForm = this.processForm.bind(this);
+                            this.changeUser = this.changeUser.bind(this); import React, { PropTypes } from 'react';
+                            import SignUpForm from '../components/signUpForm.jsx';
+
+
+                            class SignUpPage extends React.Component {
+                              constructor(props, context) {
+                                super(props, context);
+
+                                this.state = {
+                                  errors: {},
+                                  user: {
+                                    email: '',
+                                    name: '',
+                                    password: ''
+                                  }
+                                };
+
+                                this.processForm = this.processForm.bind(this);
+                                this.changeUser = this.changeUser.bind(this);
+                                email: '',
+                                  name: '',
+                                    password: ''
+                              }
+                            };
+
+                            this.processForm = this.processForm.bind(this);
+                            this.changeUser = this.changeUser.bind(this);
+                          }
+
+                          processForm(event) {
+                            event.preventDefault();
+                            const name = encodeURIComponent(this.state.user.name);
+                            const email = encodeURIComponent(this.state.user.email);
+                            const password = encodeURIComponent(this.state.user.password);
+                            const formData = `name=${name}&email=${email}&password=${password}`;
+
+                            const xhr = new XMLHttpRequest();
+                            xhr.open('post', '/auth/signup');
+                            xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+                            xhr.responseType = 'json';
+                            xhr.addEventListener('load', () => {
+                              if (xhr.status === 200) {
+                                this.setState({
+                                  errors: {}
+                                });
+                                localStorage.setItem('successMessage', xhr.response.message);
+                                this.context.router.replace('/login');
+                              } else {
+                                const errors = xhr.response.errors ? xhr.response.errors : {};
+                                errors.summary = xhr.response.message;
+
+                                this.setState({
+                                  errors
+                                });
+                              }
+                            });
+                            xhr.send(formData);
+                          }
+
+                          changeUser(event) {
+                            const field = event.target.name;
+                            const user = this.state.user;
+                            user[field] = event.target.value;
+
+                            this.setState({
+                              user
+                            });
+                          }
+
+                          render() {
+                            return (
+                              <SignUpForm
+                                onSubmit={this.processForm}
+                                onChange={this.changeUser}
+                                errors={this.state.errors}
+                                user={this.state.user}
+                              />
+                            );
+                          }
+
+                        }
+
+                        SignUpPage.contextTypes = {
+                          router: PropTypes.object.isRequired
+                        };
+
+                        export default SignUpPage;
